d6c0d89f47e7c0c29d3736f3d7dd2c50d3a7a35e
Mohamed Hassanin <mhassanin@microsoft.com>
2022-01-18 20:46:44 -0500

    build and deploy .NET project

--- RetrospectiveExtension.Backend/deploy/env_setup.sh
+++ RetrospectiveExtension.Backend/deploy/env_setup.sh
@@ -7,7 +7,7 @@
 ## Resources include:
 ## - Resource group
 ## - SignalR service
-## - VM
+## - Azure Web App
 ##
 
 (
@@ -20,18 +20,21 @@
     cd "$(dirname "$0")/.." || exit
     set -euo pipefail
 
+    # Set Resource(s) suffix and location
+    resource_name_suffix="${RESOURCE_NAME_SUFFIX}"
+    resource_group="rg-${RESOURCE_NAME_SUFFIX}"
+    location="${LOCATION}"
+
+    
+    
+    # Login to Azure via Service Principal
+
     # Set service principal information
     echo "#### Exporting SP as environment variables ####"
     subscription_id="${SUBSCRIPTION_ID}"
     tenant_id="${TENANT_ID}"
     service_principal_id="${SERVICE_PRINCIPAL_ID}"
     service_principal_secret="${SERVICE_PRINCIPAL_SECRET}"
-
-    resource_name_suffix="${RESOURCE_NAME_SUFFIX}"
-    resource_group="rg-${RESOURCE_NAME_SUFFIX}"
-    location="${LOCATION}"
-
-    # Login to Azure via Service Principal
     echo "#### Attempting az login via service principal ####"
     az login \
         --service-principal \
@@ -67,19 +70,48 @@
     az webapp create \
         --name "app-${resource_name_suffix}" \
         --resource-group "$resource_group" \
-        --plan "plan-${resource_name_suffix}"
+        --plan "plan-${resource_name_suffix}"\
+        --runtime "DOTNET|5.0"
 
     signalr_connection_string=$( \
         az signalr key list \
             --name "sigr-${resource_name_suffix}" \
             --resource-group "$resource_group" \
-            --query primaryKey \
+            --query primaryConnectionString \
             --output tsv)
 
+    echo "####  Deploying App Settings #######"
     # https://docs.microsoft.com/en-us/cli/azure/webapp/config/appsettings?view=azure-cli-latest#az-webapp-config-appsettings-set
     # Create WebApp AppSettings
     az webapp config appsettings set \
         --resource-group "$resource_group" \
         --name "app-${resource_name_suffix}" \
         --settings Azure__SignalR__ConnectionString=$signalr_connection_string "@dev_certs.json" "@allowed_origins.json"
+
+    #Create Output directory to publish the dotnet project artifacts to be published to the Azure Web Apps Instance
+
+    echo "###    Publishing backend project      #####"
+    dotnet build
+    # Publish the Dotnet project
+    dotnet publish -c Release 
+
+    cd  bin/Release/net5/publish/
+    # Zip the deployed artifact
+    zip -r "../../../website.zip" .
+
+    cd ../../../..
+   
+    # https://docs.microsoft.com/en-us/cli/azure/webapp?view=azure-cli-latest#az-webapp-deploy
+    # Deploy the web app
+    az webapp deploy --resource-group "$resource_group" \
+        --name "app-${resource_name_suffix}" --src-path "bin/website.zip"
+
+    backend_service_url=$( az webapp show  \
+        --resource-group "$resource_group" \
+        --name "app-${resource_name_suffix}" \
+         --query defaultHostName --output tsv)
+
+    backend_health_check="https://${backend_service_url}/health"
+   
+    echo "backend service deployed at ${backend_service_url}, to check the health of the deployment visit ${backend_health_check}"
 )
