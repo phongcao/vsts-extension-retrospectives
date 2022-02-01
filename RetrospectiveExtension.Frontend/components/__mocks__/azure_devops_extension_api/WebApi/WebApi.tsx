186721f75b26e4b543d2eff903f5408260c4cddb
Marisa Ashour <marisaashour@microsoft.com>
2022-02-01 14:11:07 -0500

    :hammer: more mocks - particularly WorkItemTrackingProcess.

---
+++ RetrospectiveExtension.Frontend/components/__mocks__/azure_devops_extension_api/WebApi/WebApi.tsx
@@ -0,0 +1,37 @@
+import {v4 as uuid} from 'uuid';
+
+export const IdentityRef = {
+    directoryAlias: "mocked directory alias",
+    id: uuid(),
+    imageUrl: "mockedImageUrl",
+    inactive: false,
+    isAadIdentity: false,
+    isContainer: false,
+    isDeletedInOrigin: false,
+    profileUrl: "mockedProfileUrl",
+    uniqueName: "mocked unique name",
+    _links: [],
+    descriptor: "mocked descriptor",
+    displayName: "mocked display name",
+    url: "mockedUrl"
+};
+
+export const JsonPatchDocument = {
+};
+
+export const JsonWebToken = {
+};
+
+export enum Operation {
+    Add = 0,
+    Remove = 1,
+    Replace = 2,
+    Move = 3,
+    Copy = 4,
+    Test = 5
+}
+
+export interface TeamMember {
+    identity: {IdentityRef},
+    isTeamAdmin: false,
+};
---
+++ RetrospectiveExtension.Frontend/components/__mocks__/azure_devops_extension_api/WorkItemTrackingProcess/WorkItemTrackingProcess.tsx
@@ -0,0 +1,10 @@
+export enum CustomizationType {
+    System = 1,
+    Inherited = 2,
+    Custom = 3
+};
+
+export enum GetProcessExpandLevel {
+    None = 0,
+    Projects = 1
+};
