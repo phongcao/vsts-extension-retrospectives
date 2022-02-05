d5109a21a2e5020cba406ee524b5249ebb2f6ca0
Andrew Francisque <anfranc@microsoft.com>
2022-02-05 18:40:52 +0000

    Revert mocking

--- RetrospectiveExtension.Frontend/components/__tests__/actionItemDisplay.test.tsx
+++ RetrospectiveExtension.Frontend/components/__tests__/actionItemDisplay.test.tsx
@@ -1,46 +1,84 @@
 import * as React from "react";
 import * as TestRenderer from "react-test-renderer";
 import ActionItemDisplay, {
-  ActionItemDisplayProps
+  ActionItemDisplayProps,
 } from "../actionItemDisplay";
-import { mockEnv } from "../__mocks__/config/environment";
-import { mockCore } from "../__mocks__/azure-devops-extension-api/Core/Core";
-import { mockCommon } from "../__mocks__/azure-devops-extension-api/Common/Common";
-import { MockSDK } from "../__mocks__/azure-devops-extension-sdk/sdk";
-
-jest.mock("../../config/environment", () => {
-  return mockEnv;
-});
+import { CommonServiceIds } from "azure-devops-extension-api";
 
 // Mock Azure DevOps Extension SDK
-jest.mock("azure-devops-extension-sdk", () => {
-  return MockSDK;
+jest.mock('azure-devops-extension-sdk', () => {
+  const mockExtensionDataService = {
+    getExtensionDataManager: jest.fn(),
+  };
+
+  const mockLocationService = {
+    getResourceAreaLocation: jest.fn().mockImplementation(() => 'https://hosturl'),
+  };
+
+  const mockProjectPageService = {
+    getProject: jest.fn().mockImplementation(() => {
+      const mockProjectInfo = {
+        id: "id",
+        name: "name",
+      };
+
+      return mockProjectInfo;
+    }),
+  };
+
+  const mockUser = {
+    id: "userId",
+  };
+
+  const mockExtensionContext = {
+    id: "contextId",
+  };
+
+  const mockSdk = {
+    getService: jest.fn().mockImplementation(id => {
+      if (id == CommonServiceIds.LocationService)
+        return mockLocationService;
+      else if (id == CommonServiceIds.ProjectPageService)
+        return mockProjectPageService;
+      else
+        return mockExtensionDataService;
+    }),
+    getUser: jest.fn().mockImplementation(() => mockUser),
+    getExtensionContext: jest.fn().mockImplementation(() => mockExtensionContext),
+    getAccessToken: jest.fn().mockImplementation(() => 'token'),
+  };
+
+  return mockSdk;
 });
 
 // Mock Azure DevOps Extension API
-jest.mock("azure-devops-extension-api/Core", () => {
+jest.mock('azure-devops-extension-api/Core', () => {
+  const mockCore = {
+    CoreRestClient: {
+      RESOURCE_AREA_ID: "resourceAreaId",
+    },
+  };
+
   return mockCore;
 });
 
-jest.mock("azure-devops-extension-api/Core/CoreClient", () => {});
-jest.mock("azure-devops-extension-api/WebApi", () => {});
-jest.mock("azure-devops-extension-api/WorkItemTracking", () => {});
-jest.mock(
-  "azure-devops-extension-api/WorkItemTracking/WorkItemTracking",
-  () => {}
-);
-jest.mock(
-  "azure-devops-extension-api/WorkItemTracking/WorkItemTrackingClient",
-  () => {
-    const mockWorkItemTrackingClient = {
-      WorkItemTrackingRestClient: {},
-    };
+jest.mock('azure-devops-extension-api/Core/CoreClient', () => {});
+jest.mock('azure-devops-extension-api/WebApi', () => {});
+jest.mock('azure-devops-extension-api/WorkItemTracking', () => {});
+jest.mock('azure-devops-extension-api/WorkItemTracking/WorkItemTracking', () => {});
+jest.mock('azure-devops-extension-api/WorkItemTracking/WorkItemTrackingClient', () => {
+  const mockWorkItemTrackingClient = {
+    WorkItemTrackingRestClient: {},
+  };
 
-    return mockWorkItemTrackingClient;
-  }
-);
+  return mockWorkItemTrackingClient;
+});
 
-jest.mock("azure-devops-extension-api/Common", () => {
+jest.mock('azure-devops-extension-api/Common', () => {
+  const mockCommon = {
+    getClient: jest.fn(),
+  };
+
   return mockCommon;
 });
 
